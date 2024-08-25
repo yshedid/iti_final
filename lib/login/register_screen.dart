698c58d861454cda7002806124bbbde0c964aec1
elkholy22 <elkholyyoussif@gmail.com>
2024-08-25 16:37:55 +0300

    some edit

--- lib/login/register_screen.dart
+++ lib/login/register_screen.dart
@@ -1,19 +1,16 @@
-import 'dart:developer';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:iti_final/helper/app_color.dart';
 import 'package:iti_final/login/login_screeen.dart';
 import 'package:iti_final/onboarding/widget/bulid_text_fromfield.dart';
 import 'package:iti_final/onboarding/widget/social_button.dart';
-
+import 'package:iti_final/screens/home_page/home_page.dart';
 
 class RegestrScreeen extends StatefulWidget {
   const RegestrScreeen({super.key});
   static String id = 'RegestrScreeen';
 
   @override
-  // ignore: library_private_types_in_public_api
   _RegestrScreeenState createState() => _RegestrScreeenState();
 }
 
@@ -22,9 +19,6 @@ class _RegestrScreeenState extends State<RegestrScreeen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController nameController = TextEditingController();
-
-  
-  
 
   @override
   Widget build(BuildContext context) {
@@ -37,8 +31,10 @@ class _RegestrScreeenState extends State<RegestrScreeen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-              const SizedBox(height: 10,),
-               const Text(
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
                   'Register Now',
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                   textAlign: TextAlign.center,
@@ -50,12 +46,24 @@ class _RegestrScreeenState extends State<RegestrScreeen> {
                   textAlign: TextAlign.center,
                 ),
                 const SizedBox(height: 32),
-                
-                 BuildTextFromfield(label: 'Name',hint: 'Full Name',controller: nameController,),
+                BuildTextFromfield(
+                  label: 'Name',
+                  hint: 'Full Name',
+                  controller: nameController,
+                ),
                 const SizedBox(height: 16),
-                 BuildTextFromfield(label: 'Email',hint:  'Email Address',controller: emailController,),
+                BuildTextFromfield(
+                  label: 'Email',
+                  hint: 'Email Address',
+                  controller: emailController,
+                ),
                 const SizedBox(height: 16),
-                 BuildTextFromfield(label: 'Password',hint:  'New Password', isPassword: true,controller: passwordController,),
+                BuildTextFromfield(
+                  label: 'Password',
+                  hint: 'New Password',
+                  isPassword: true,
+                  controller: passwordController,
+                ),
                 const SizedBox(height: 16),
                 Row(
                   children: [
@@ -70,25 +78,38 @@ class _RegestrScreeenState extends State<RegestrScreeen> {
                             agreeToTerms = value!;
                           });
                         },
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4)),
                       ),
                     ),
                     const SizedBox(width: 8),
-                    const Text('I agree to the terms & conditions', style: TextStyle(fontSize: 14)),
+                    const Text('I agree to the terms & conditions',
+                        style: TextStyle(fontSize: 14)),
                   ],
                 ),
-               const SizedBox(height: 24),
+                const SizedBox(height: 24),
                 ElevatedButton(
-
-                  child: const Text('Register', style: TextStyle(fontSize: 16)),
-                  onPressed: ()
-                   {
-                     if ( (emailController.text != '') &&  (passwordController.text != '')) {
-                       // Perform login
-                       var y =FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text,
-                           password: passwordController.text);
-                     }
-                   },
+                  child: Text('Register', style: TextStyle(fontSize: 16)),
+                  onPressed: () {
+                    if (emailController.text.isNotEmpty &&
+                        passwordController.text.isNotEmpty) {
+                      try {
+                        FirebaseAuth.instance.createUserWithEmailAndPassword(
+                          email: emailController.text,
+                          password: passwordController.text,
+                        );
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => MainPageContent(),
+                          ),
+                        );
+                      } catch (e) {
+                        print('Error: $e');
+                      }
+                    } else {
+                      print('Email or password cannot be empty');
+                    }
+                  },
                   style: ElevatedButton.styleFrom(
                     foregroundColor: myWhithe,
                     backgroundColor: myBlue,
@@ -98,43 +119,48 @@ class _RegestrScreeenState extends State<RegestrScreeen> {
                     ),
                   ),
                 ),
-               const SizedBox(height: 24),
+                const SizedBox(height: 24),
                 Row(
                   children: [
                     Expanded(child: Divider(color: Colors.grey[300])),
                     const Padding(
-                      padding:  EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text('Or', style: TextStyle(color: myBlack, fontSize: 14)),
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text('Or',
+                          style: TextStyle(color: myBlack, fontSize: 14)),
                     ),
                     const Expanded(child: Divider(color: myGray)),
                   ],
                 ),
-               const SizedBox(height: 24),
-               Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    socialButton('assets/images/apple.jpg'),
-                     const SizedBox(width: 20),
-                     socialButton('assets/images/google.jpg'),
-                     const SizedBox(width: 20),
-                    socialButton('assets/images/facebook.jpg',),
-                  ],
-                ),
-               const SizedBox(height: 24),
+                const SizedBox(height: 24),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('Already Have An Account?', style: TextStyle(color: Colors.grey[600], fontSize: 14)),
+                    socialButton('assets/images/apple.jpg'),
+                    const SizedBox(width: 20),
+                    socialButton('assets/images/google.jpg'),
+                    const SizedBox(width: 20),
+                    socialButton(
+                      'assets/images/facebook.jpg',
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Already Have An Account?',
+                        style: TextStyle(color: Colors.grey[600], fontSize: 14)),
                     TextButton(
-                      child:const Text('Login', style: TextStyle(color: myBlue, fontWeight: FontWeight.bold)),
-                      onPressed: ()
-                       {
+                      child: const Text('Login',
+                          style: TextStyle(
+                              color: myBlue, fontWeight: FontWeight.bold)),
+                      onPressed: () {
                         Navigator.of(context).push(
                           MaterialPageRoute(
                             builder: (context) => const LoginScreeen(),
                           ),
                         );
-                       },
+                      },
                     ),
                   ],
                 ),
@@ -145,5 +171,4 @@ class _RegestrScreeenState extends State<RegestrScreeen> {
       ),
     );
   }
-
 }
